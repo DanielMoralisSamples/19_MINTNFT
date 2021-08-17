6d026ca0753503e9f5469dd16bf49e1cfed8e026
Daniel Herrera <mayan.bridge.consulting@gmail.com>
2021-08-17 15:11:56 +0800

    project of august 17 2021

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# Instructions
+1. Requires python3, if you don't have it there are multiple resources online on how to install it according to your platform.
+2. Set a virtual environment with `Python3 -m venv venv`
+3. Initialize your virtual environment with `source venv/bin/activate` (For Mac and Linux).
+4. Intall the dependencies with `pip install -r requirements.txt`
+5. Play with it
---
+++ app/static/js/logic.js
@@ -0,0 +1,72 @@
+Moralis.initialize(""); // Application id from moralis.io
+Moralis.serverURL = ""; //Server url from moralis.io
+
+const nft_contract_address = "0x0Fb6EF3505b9c52Ed39595433a21aF9B5FCc4431" //NFT Minting Contract Use This One "Batteries Included", code of this contract is in the github repository under contract_base for your reference.
+
+const web3 = new Web3(window.ethereum);
+
+//frontend logic
+
+async function login(){
+  document.getElementById('submit').setAttribute("disabled", null);
+  document.getElementById('username').setAttribute("disabled", null);
+  document.getElementById('useremail').setAttribute("disabled", null);
+  Moralis.Web3.authenticate().then(function (user) {
+      user.set("name",document.getElementById('username').value);
+      user.set("email",document.getElementById('useremail').value);
+      user.save();
+      document.getElementById("upload").removeAttribute("disabled");
+      document.getElementById("file").removeAttribute("disabled");
+      document.getElementById("name").removeAttribute("disabled");
+      document.getElementById("description").removeAttribute("disabled");
+  })
+}
+
+async function upload(){
+  const fileInput = document.getElementById("file");
+  const data = fileInput.files[0];
+  const imageFile = new Moralis.File(data.name, data);
+  document.getElementById('upload').setAttribute("disabled", null);
+  document.getElementById('file').setAttribute("disabled", null);
+  document.getElementById('name').setAttribute("disabled", null);
+  document.getElementById('description').setAttribute("disabled", null);
+  await imageFile.saveIPFS();
+  const imageURI = imageFile.ipfs();
+  const metadata = {
+    "name":document.getElementById("name").value,
+    "description":document.getElementById("description").value,
+    "image":imageURI
+  }
+  const metadataFile = new Moralis.File("metadata.json", {base64 : btoa(JSON.stringify(metadata))});
+  await metadataFile.saveIPFS();
+  const metadataURI = metadataFile.ipfs();
+  const txt = await mintToken(metadataURI).then(notify)
+}
+
+async function mintToken(_uri){
+  const encodedFunction = web3.eth.abi.encodeFunctionCall({
+    name: "mintToken",
+    type: "function",
+    inputs: [{
+      type: 'string',
+      name: 'tokenURI'
+      }]
+  }, [_uri]);
+
+  const transactionParameters = {
+    to: nft_contract_address,
+    from: ethereum.selectedAddress,
+    data: encodedFunction
+  };
+  const txt = await ethereum.request({
+    method: 'eth_sendTransaction',
+    params: [transactionParameters]
+  });
+  return txt
+}
+
+async function notify(_txt){
+  document.getElementById("resultSpace").innerHTML =  
+  `<input disabled = "true" id="result" type="text" class="form-control" placeholder="Description" aria-label="URL" aria-describedby="basic-addon1" value="Your NFT was minted in transaction ${_txt}">`;
+} 
+
