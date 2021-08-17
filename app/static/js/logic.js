a93fa2e22ea7fc9461a110b8331f71bf959f3e12
Daniel Herrera <mayan.bridge.consulting@gmail.com>
2021-08-17 15:22:28 +0800

    project of august 17 2021

--- app/static/js/logic.js
+++ app/static/js/logic.js
@@ -1,7 +1,13 @@
 Moralis.initialize(""); // Application id from moralis.io
 Moralis.serverURL = ""; //Server url from moralis.io
 
-const nft_contract_address = "0x0Fb6EF3505b9c52Ed39595433a21aF9B5FCc4431" //NFT Minting Contract Use This One "Batteries Included", code of this contract is in the github repository under contract_base for your reference.
+const nft_contract_address = "" //NFT Minting Contract Use This One "Batteries Included", code of this contract is in the github repository under contract_base for your reference.
+/*
+Available deployed contracts
+Ethereum Rinkeby 0x0Fb6EF3505b9c52Ed39595433a21aF9B5FCc4431
+Polygon Mumbai 0x351bbee7C6E9268A1BF741B098448477E08A0a53
+BSC Testnet 0x88624DD1c725C6A95E223170fa99ddB22E1C6DDD
+*/
 
 const web3 = new Web3(window.ethereum);
 
